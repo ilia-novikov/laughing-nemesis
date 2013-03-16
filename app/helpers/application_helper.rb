9477b0c90381c764691f8a491c17777d0f91abdf
Ilia Novikov <ilia.novikov@live.ru>
2013-03-16 21:40:09 +0400

    Created header navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+	# Return a title on a per-page basis.
+	def title
+		base_title = "Ruby testing"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
+	def classes
+		if @title == "Home"
+			return {:home => "active", :contact => "", :about => ""}
+		end
+		if @title == "Contact"
+			return {:home => "", :contact => "active", :about => ""}
+		end		
+		if @title == "About"
+			return {:home => "", :contact => "", :about => "active"}
+		end
+	end
 end
